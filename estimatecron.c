60ad889067dd2915552ea4dd08a186783094d32a
Nik <nikolag000@hotmail.com>
2022-09-09 12:39:47 +0800

    struct

--- estimatecron.c
+++ estimatecron.c
@@ -11,100 +11,101 @@ struct Process {
    char  name[40];		// process name
    char  start_time[15];	// in format: (string) s/m/h/m/d
    int   estimate;		// process length in minutes
+   int num_lines;		// number of processes
 };
 
 
-struct Process *file_processing(char *filename) {
+struct Process *file_processing(char *crontab, char *estimates) {
 
 	// storage for lines from file
-	char data[MAX_LINES][MAX_LEN];
+	char cron_data[MAX_LINES][MAX_LEN];
 	
 	// open file
-	FILE   *file = fopen(filename, "r");
+	FILE   *cron_file = fopen(crontab, "r");
 	
 	//  check if file was opened successfully
-	if(file == NULL) {
-		printf( "cannot open file '%s'\n", filename);
+	if(cron_file == NULL) {
+		printf( "cannot open file '%s'\n", crontab);
 		exit(EXIT_FAILURE);
 	}
 	
-	int line = 0;
+	int cron_line = 0;
 	
 	// check for EOF or error while reading
-	while (!feof(file) && !ferror(file)) {
+	while (!feof(cron_file) && !ferror(cron_file)) {
 		// check if number of lines < MAX_LINES
-		if (fgets(data[line], MAX_LEN, file) != NULL) {
+		if (fgets(cron_data[cron_line], MAX_LEN, cron_file) != NULL) {
 			// increment line
-			line++;
+			cron_line++;
 		}
 	}
 	
 	//  close file
-	fclose(file);
+	fclose(cron_file);
 	
 	// clean lines
-	char processes[MAX_LINES][MAX_LEN];
+	char cron_processes[MAX_LINES][MAX_LEN];
 	
-	int clean_line = 0;
+	int cron_clean_line = 0;
 	
-	for (int i=0;i<line;i++) {
+	for (int i=0;i<cron_line;i++) {
 		for (int j = 0; j < MAX_LINES; j++) {
-			if (data[i][j] == ' ') {
+			if (cron_data[i][j] == ' ') {
 				continue;
 			}
-			else if (data[i][j] == '#') {
+			else if (cron_data[i][j] == '#') {
 				break;
 			}
-			else if (data[i][j] == '\n') {
-  			        strcpy(processes[clean_line], data[i]);
-				clean_line++;
+			else if (cron_data[i][j] == '\n') {
+  			        strcpy(cron_processes[cron_clean_line], cron_data[i]);
+				cron_clean_line++;
 			}
 		}
 	}
 	
-	char times[clean_line][100];
-	for (int z=0;z<clean_line;z++) {
-  		times[z][0] = '\0';
+	char cron_times[cron_clean_line][100];
+	for (int z=0;z<cron_clean_line;z++) {
+  		cron_times[z][0] = '\0';
 	}
 	
-	for (int z=0;z<clean_line;z++) {
+	for (int z=0;z<cron_clean_line;z++) {
 		int x = 0;
 		int space_counter = 0;
-		while ( (processes[z][x] != '\0') && (space_counter < 5) ) {
-			if (processes[z][x] == ' ') {
-				strcat(times[z], "/");
+		while ( (cron_processes[z][x] != '\0') && (space_counter < 5) ) {
+			if (cron_processes[z][x] == ' ') {
+				strcat(cron_times[z], "/");
 				space_counter++;
 				x++;
 			}
 			else {
-				strncat(times[z], &processes[z][x], 1);
+				strncat(cron_times[z], &cron_processes[z][x], 1);
 				x++;
 			}
     		}
     	}
     	
-    	char names[clean_line][40];
-    	for (int z=0;z<clean_line;z++) {
-    		names[z][0] = '\0';
+    	char cron_names[cron_clean_line][40];
+    	for (int z=0;z<cron_clean_line;z++) {
+    		cron_names[z][0] = '\0';
     	}
     	
-	for (int z=0;z<clean_line;z++) {
+	for (int z=0;z<cron_clean_line;z++) {
 		int x = 0;
 		int space_counter = 0;
-		while ( (processes[z][x] != '\n') ) {
+		while ( (cron_processes[z][x] != '\n') ) {
 			if (space_counter > 5) {
-				if (processes[z][x] == ' ') {
+				if (cron_processes[z][x] == ' ') {
 					x++;
 					space_counter++;
 					continue;
 				}
 				else {
-					strncat(names[z], &processes[z][x], 1);
+					strncat(cron_names[z], &cron_processes[z][x], 1);
 					x++;
 				}
 			}
 			else {
-				if (processes[z][x] == ' ') {
+				if (cron_processes[z][x] == ' ') {
 					x++;
 					space_counter++;
 				}
@@ -114,13 +115,122 @@ struct Process *file_processing(char *filename) {
 			}
     		}
     	}
-
-	struct Process *all_proc = malloc(sizeof(struct Process) * clean_line);       
 	
-	for (int i=0;i<clean_line;i++) {
-		strcpy( all_proc[i].name, names[i]);
-		strcpy( all_proc[i].start_time, times[i]);
-		all_proc[i].estimate = 0;
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// storage for lines from file
+	char esti_data[MAX_LINES][MAX_LEN];
+	
+	// open file
+	FILE   *esti_file = fopen(estimates, "r");
+	
+	//  check if file was opened successfully
+	if(esti_file == NULL) {
+		printf( "cannot open file '%s'\n", estimates);
+		exit(EXIT_FAILURE);
+	}
+	
+	int esti_line = 0;
+	
+	// check for EOF or error while reading
+	while (!feof(esti_file) && !ferror(esti_file)) {
+		// check if number of lines < MAX_LINES
+		if (fgets(esti_data[esti_line], MAX_LEN, esti_file) != NULL) {
+			// increment line
+			esti_line++;
+		}
+	}
+	
+	//  close file
+	fclose(esti_file);
+
+	// clean lines
+	char esti_processes[MAX_LINES][MAX_LEN];
+	
+	int esti_clean_line = 0;
+	
+	for (int i=0;i<esti_line;i++) {
+		for (int j = 0; j < MAX_LINES; j++) {
+			if (esti_data[i][j] == ' ') {
+				continue;
+			}
+			else if (esti_data[i][j] == '#') {
+				break;
+			}
+			else if (esti_data[i][j] == '\n') {
+  			        strcpy(esti_processes[esti_clean_line], esti_data[i]);
+				esti_clean_line++;
+			}
+		}
+	}
+	
+	char esti_names[esti_clean_line][40];
+    	for (int z=0;z<esti_clean_line;z++) {
+    		esti_names[z][0] = '\0';
+    	}
+    	
+	for (int z=0;z<esti_clean_line;z++) {
+		int x = 0;
+		int space_counter = 0;
+		while ( (space_counter < 1) ) {
+			if (esti_processes[z][x] == ' ') {
+				x++;
+				space_counter++;
+			}
+			else {
+				strncat(esti_names[z], &esti_processes[z][x], 1);
+				x++;
+			}
+		}
+	}
+	
+	char esti_times[esti_clean_line][40];
+    	for (int z=0;z<esti_clean_line;z++) {
+    		esti_times[z][0] = '\0';
+    	}
+    	
+	for (int z=0;z<esti_clean_line;z++) {
+		int x = 0;
+		int space_counter = 0;
+		while ( (esti_processes[z][x] != '\n') ) {
+			if (space_counter >= 1) {
+				if (esti_processes[z][x] == ' ') {
+					x++;
+					space_counter++;
+				}
+				else {
+					strncat(esti_times[z], &esti_processes[z][x], 1);
+					x++;
+				}
+			}
+			else {
+				if (esti_processes[z][x] == ' ') {
+					x++;
+					space_counter++;
+				}
+				else {
+					x++;
+				}
+			}
+		}
+	}
+
+	struct Process *all_proc = malloc(sizeof(struct Process) * cron_clean_line);       
+	
+	for (int i=0;i<cron_clean_line;i++) {
+		strcpy( all_proc[i].name, cron_names[i]);
+		strcpy( all_proc[i].start_time, cron_times[i]);
+		//all_proc[i].estimate = 0;
+		all_proc[i].num_lines = cron_clean_line;
+	}
+	
+	for (int i=0;i<cron_clean_line;i++) {
+		for (int j=0;j<esti_clean_line;j++) {
+			if (strcmp(all_proc[i].name, esti_names[j]) == 0) {
+				all_proc[i].estimate = atoi(esti_times[j]);
+			}
+		}
 	}
 		
 	return all_proc;
@@ -199,23 +309,16 @@ int main(int argc, char *argv[]) {
 		
 		printf("\n");
 		
-		struct Process *all_proc = file_processing(argv[2]); 		
+		struct Process *all_proc = file_processing(argv[2], argv[3]); 		
 				
-		for (int i=0;i<5;i++) {
-			printf("##   %s   ##   %s   ##   %i   ##\n\n", all_proc[i].name, 
-							       	       all_proc[i].start_time, 
-						                       all_proc[i].estimate);
+		for (int i=0;i<all_proc[i].num_lines;i++) {
+			printf("##   %s   ##   %s   ##   %i   ##   %i   ##\n\n", all_proc[i].name, 
+							       	                 all_proc[i].start_time, 
+						                                 all_proc[i].estimate,
+						                                 all_proc[i].num_lines);
 		}
 
 		free(all_proc);
-		
-		//file_processing(argv[2]);
-		
-		printf("\n\n");
-		
-		//file_processing(argv[3]);
-		
-		printf("\n");
 	
 		exit(EXIT_SUCCESS);
 	}
